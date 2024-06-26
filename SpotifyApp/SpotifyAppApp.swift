35bc45876d232cc711e888c0c1df0d9b17bcac4e
Murad <Sharudinov001@yandex.ru>
2024-06-26 16:20:10 +0300

    Initial Commit

---
+++ SpotifyApp/SpotifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAppApp.swift
+//  SpotifyApp
+//
+//  Created by Мурад on 26.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
